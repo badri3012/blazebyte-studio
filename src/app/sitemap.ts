47c75299b6aefe22216f4c21202d200a2380fdfb
Badri Narayanan <gamersdevil679@gmail.com>
2026-09-01 03:39:04 +0530

    add SEO page to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,11 +10,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 1,
     },
-    {
-      url: `${baseUrl}/services`,
+  
+      {url: `${baseUrl}/services`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
+    },
+      {
+      url: `${baseUrl}/services/seo`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
     },
     {
       url: `${baseUrl}/packages`,
